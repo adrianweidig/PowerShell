f299df4de0afff3c4264ac010d3c152c5e23105f
Adrian Weidig <70036819+adrianweidig@users.noreply.github.com>
2023-07-13 11:42:24 +0200

    GUI Grundlagen mit WPF und Winform

---
+++ GUI/WPF/Grundlagen.ps1
@@ -0,0 +1,50 @@
+﻿[xml]$XAML = @"
+<Window x:Class="WpfApplication.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="MainWindow" Height="300" Width="400">
+    <Grid>
+        <Button x:Name="MeinButton" Content="Klick mich!" HorizontalAlignment="Center" VerticalAlignment="Center"/>
+        <Label x:Name="MeinLabel" Content="Hallo, Welt!" HorizontalAlignment="Center" VerticalAlignment="Top" Margin="0,50,0,0"/>
+    </Grid>
+</Window>
+"@ -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window' #-replace wird benötigt, wenn XAML aus Visual Studio kopiert wird.
+
+##############################################
+# Abschnitt 1: Einführung in WPF
+##############################################
+
+Write-Output "`n### Abschnitt 1: Einführung in WPF ###`n"
+
+# Erst hiermit ist WPF wie beim Import eines Moduls aufrufbar
+Add-Type -AssemblyName PresentationFramework
+
+# XAML-Daten aus Datei laden (Auskommentieren, falls InCode Variante (siehe oben) genutzt werden will)
+$XAML = [xml](Get-Content "E:\Programmierung\PowerShell\GUI\WPF\XAMLLayout.xaml")
+
+# XAML-Daten laden
+$window=[Windows.Markup.XamlReader]::Load( (New-Object System.Xml.XmlNodeReader $XAML) )
+
+# Fenster anzeigen
+$result = $window.ShowDialog()
+
+##############################################
+# Abschnitt 2: Hinzufügen von Steuerelementen
+##############################################
+
+Write-Output "`n### Abschnitt 2: Hinzufügen von Steuerelementen ###`n"
+
+# XAML-Datei laden
+$window=[Windows.Markup.XamlReader]::Load( (New-Object System.Xml.XmlNodeReader $XAML) )
+
+# Steuerelemente suchen
+$button = $window.FindName("MeinButton")
+$label = $window.FindName("MeinLabel")
+
+# Ereignisbehandlung für den Button hinzufügen
+$button.Add_Click({
+    $label.Content = "Button wurde geklickt!"
+})
+
+# Fenster anzeigen
+$result = $window.ShowDialog()
---
+++ GUI/Winform/Grundlagen.ps1
@@ -0,0 +1,68 @@
+﻿##############################################
+# Abschnitt 1: Einführung in WinForms
+##############################################
+
+Write-Output "`n### Abschnitt 1: Einführung in WinForms ###`n"
+
+Add-Type -AssemblyName System.Windows.Forms
+
+# Neues Windows-Formular erstellen
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Mein erstes WinForm"
+$form.Size = New-Object System.Drawing.Size(400, 300)
+$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::Sizable
+
+# Formular anzeigen
+$form.ShowDialog()
+
+##############################################
+# Abschnitt 2: Hinzufügen von Steuerelementen
+##############################################
+
+Write-Output "`n### Abschnitt 2: Hinzufügen von Steuerelementen ###`n"
+
+# Button hinzufügen
+$button = New-Object System.Windows.Forms.Button
+$button.Text = "Klick mich!"
+$button.Location = New-Object System.Drawing.Point(50, 50)
+$form.Controls.Add($button)
+
+# Label hinzufügen
+$label = New-Object System.Windows.Forms.Label
+$label.Text = "Hallo, Welt!"
+$label.Location = New-Object System.Drawing.Point(50, 100)
+$form.Controls.Add($label)
+
+# Formular anzeigen
+$form.ShowDialog()
+
+##############################################
+# Abschnitt 3: Ereignisse und Aktionen
+##############################################
+
+Write-Output "`n### Abschnitt 3: Ereignisse und Aktionen ###`n"
+
+# Ereignisbehandlung für den Button hinzufügen
+$button.Add_Click({
+    $label.Text = "Button wurde geklickt!"
+})
+
+# Formular anzeigen
+$form.ShowDialog()
+
+##############################################
+# Abschnitt 4: Dialogfenster anzeigen
+##############################################
+
+Write-Output "`n### Abschnitt 4: Dialogfenster anzeigen ###`n"
+
+# Dialogfenster anzeigen
+$result = [System.Windows.Forms.MessageBox]::Show("Möchten Sie fortfahren?", "Bestätigung", `
+    [System.Windows.Forms.MessageBoxButtons]::OKCancel)
+
+# Ergebnis überprüfen
+if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+    Write-Output "OK wurde ausgewählt."
+} else {
+    Write-Output "Abbrechen wurde ausgewählt."
+}
